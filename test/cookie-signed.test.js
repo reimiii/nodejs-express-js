28c2803f45b8e616d804f2cc8cf72494d7b99a63
Hilmi AM <imiia75775@proton.me>
2024-09-09 02:05:03 +0700

    sign cookie

---
+++ test/cookie-signed.test.js
@@ -0,0 +1,32 @@
+import express from "express";
+import request from "supertest";
+import cookieParser from "cookie-parser";
+
+const app = express();
+app.use(cookieParser("CONTOHRAHASIA"));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    const name = req.signedCookies["Login"];
+    res.send(`Hello ${name}`);
+});
+
+app.post('/login', (req, res) => {
+    const name = req.body.name;
+    res.cookie("Login", name, {path: "/", signed: true});
+    res.send(`Hello ${name}`);
+});
+
+test("Test Cookie Read", async () => {
+    const response = await request(app).get("/")
+        .set("Cookie", "Login=s%3AMe.Qm8dbqGn2q95n7aYLtUezXvev4KHDkXpr34fXZy25vo; Path=/");
+    expect(response.text).toBe("Hello Me");
+});
+
+test("Test Cookie Write", async () => {
+    const response = await request(app).post("/login")
+        .send({name: "Me"});
+    console.info(response.get("Set-Cookie"));
+    expect(response.get("Set-Cookie").toString()).toContain("Me");
+    expect(response.text).toBe("Hello Me");
+});
